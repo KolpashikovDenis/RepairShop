586a3a1c9a6b9876d735743856e26fa0636be1cd
openSUSE <kdv-78@yandex.ru>
2018-01-15 00:00:58 +0400

    Добавлены некоторые поля в класс Pump

--- src/ru/denis/javafast/repairshop/equipment/Pump.java
+++ src/ru/denis/javafast/repairshop/equipment/Pump.java
@@ -1,4 +1,7 @@
 package ru.denis.javafast.repairshop.equipment;
 
 public class Pump {
+    private boolean isShaftBroken;
+    private boolean isBearingBroken;
+    private boolean isImpellerBroken;
 }
